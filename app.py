308861537f90201369851e148f82defd533ff9da
JeremieKoehler <jeremiekoehler@live.com>
2022-12-11 22:31:41 -0800

    AddToCartP1

--- app.py
+++ app.py
@@ -7,8 +7,18 @@ tax_rate = 0.12
 # cart is a dictionary of item id's to a number which is how
 # many of that item has been added to cart
 cart = {
-    0: 1,
-    1: 3
+    0: 0,
+    1: 0, 
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+    7: 0,
+    8: 0,
+    9: 0,
+    10: 0,
+    11: 0
 }
 
 # the catalogue is the list of all items in our store
@@ -17,14 +27,64 @@ cart = {
 catalogue = [
     {
         "id": 0,
-        "name": 'Hermes Maroon Leather Bag - "Nancy"',
-        "price": 25000
+        "name": 'A',
+        "price": 6
     },
     {
         "id": 1,
-        "name": 'LoveShackFancy Ruffle Maidy Dress with Spaghetti Straps - "Flona"',
-        "price": 425
+        "name": 'B',
+        "price": 7
     },
+    {
+        "id": 2,
+        "name": 'C',
+        "price": 8
+    },
+    {
+        "id": 3,
+        "name": 'D',
+        "price": 9
+    },
+    {
+        "id": 4,
+        "name": 'a',
+        "price": 11
+    },
+    {
+        "id": 5,
+        "name": 'b',
+        "price": 12
+    },
+    {
+        "id": 6,
+        "name": 'c',
+        "price": 13
+    },
+    {
+        "id": 7,
+        "name": 'd',
+        "price": 14
+    },
+    {
+        "id": 8,
+        "name": 'one',
+        "price": 1
+    },
+    {
+        "id": 9,
+        "name": 'two',
+        "price": 20
+    },
+    {
+        "id": 10,
+        "name": 'three',
+        "price": 300
+    },
+    {
+        "id": 11,
+        "name": 'four',
+        "price": 4000
+    }
 ]
 
 def totalPrice():
